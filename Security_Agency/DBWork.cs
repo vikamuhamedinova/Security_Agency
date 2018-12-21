e76ad13f7a45db0cd0f0e93d60bd4b9d8b808cd6
vikamuhamedinova <vikamaha@yandex.ru>
2018-12-21 22:22:58 +0700

    Создано подключение к базе

---
+++ Security_Agency/DBWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Security_Agency
+{
+    public class DBWork
+    {
+        private static DBWork instance = null;
+        public static NpgsqlConnection _connection;
+
+        public static DBWork CreateDBWork(string host, string port, string username,
+            string password, string database, string sslMode = "Require",
+            string trustServerCertificate = "true")
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            string connection = String.Format("Host={0}; Port={1}; Username={2}; " +
+                "Password={3}; Database={4}; SSL Mode={5}; Trust Server Certificate={6}",
+                host, port, username, password, database, sslMode, trustServerCertificate);
+
+            return new DBWork(connection);
+
+        }
+
+        public DBWork(string connectionString)
+        {
+            _connection = new NpgsqlConnection(connectionString);
+            _connection.Open();
+        }
+
+        ~DBWork()
+        {
+            _connection.Close();
+        }
+
+        public static string SelectFam()
+        {
+            NpgsqlCommand com = new NpgsqlCommand("select \"Surname\" from public.\"Client\" where public.\"Client\".\"PK_Client\"=1", _connection);
+            string result = com.ExecuteScalar().ToString(); ;
+            return result;
+        }
+    }
+}
---
+++ Security_Agency/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Security_Agency
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        { 
+            InitializeComponent();
+        }
+        public static DBWork DBC { private set; get; }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DBC = DBWork.CreateDBWork(
+                    "192.168.186.161",
+                    "5432",
+                    "bd_admin",
+                    "Qwerty12345",
+                    "testdb"
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+                Environment.Exit(1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = DBWork.SelectFam();
+        }
+    }
+}
