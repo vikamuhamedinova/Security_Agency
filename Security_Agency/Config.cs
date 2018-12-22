962c0c0b885d81c3c24336e16649bb0339c85c13
vikamuhamedinova <vikamaha@yandex.ru>
2018-12-22 21:23:09 +0700

    Создана конфигурация для подключения к базе

---
+++ Security_Agency/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security_Agency
+{
+    public static class Config
+    {
+        public const string host = "192.168.186.161";
+        public const string port = "5432";
+        public const string username = "bd_admin";
+        public const string password = "Qwerty12345";
+        public const string database = "testdb";
+                 
+    }
+}
