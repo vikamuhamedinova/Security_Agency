29d76063d67e8f20c13e5d3c83a851a3a3a8effe
Dartkyn <alexeypantera96@gmail.com>
2018-12-23 18:54:29 +0700

    Отредактирована форма ввода клиента

--- Security_Agency/AddClient.cs
+++ Security_Agency/AddClient.cs
@@ -16,5 +16,15 @@ namespace Security_Agency
         {
             InitializeComponent();
         }
+
+        private void ButtonAddClient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonCancelAddClient_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
