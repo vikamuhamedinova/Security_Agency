d9df288ae5c28d38c3889c70ebf430b5d1892eb8
Dartkyn <alexeypantera96@gmail.com>
2018-12-24 16:30:06 +0700

    Добавление сотрудника и должности

--- Security_Agency/AddEmployee.cs
+++ Security_Agency/AddEmployee.cs
@@ -16,5 +16,83 @@ namespace Security_Agency
         {
             InitializeComponent();
         }
+
+        private void ClearForm()
+        {
+            textBoxSurnameInput.Text = "";
+            textBoxNameInput.Text = "";
+            textBoxMiddleNameInput.Text = "";
+            maskedtextBoxInputPassID.Text = "";
+            maskedTextBoxInputINN.Text = "";
+        }
+        private void ButtonCancelAddEmployee_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите отменить добавление?", "Отмена добавления",
+                            MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+        // убирает все пустые значения, выполняет преобразования к строке или к дате
+        private Dictionary<string, string> PrepareData(Dictionary<string, string> vals)
+        {
+            IList<string> keysToRemove = new List<string>();
+            foreach (var pair in vals)
+            {
+                if (vals[pair.Key] == "")
+                    keysToRemove.Add(pair.Key);
+            }
+            foreach (var key in keysToRemove)
+            {
+                vals.Remove(key);
+            }
+            var newDict = new Dictionary<string, string>();
+            return newDict;
+        }
+        private void ButtonAddEmployee_Click(object sender, EventArgs e)
+        {
+            if (textBoxSurnameInput.Text == "" || textBoxNameInput.Text == ""
+               || maskedtextBoxInputPassID.Text == "" || maskedTextBoxInputINN.Text == "")
+            {
+                MessageBox.Show("Не заполнено одно из обязательных полей");
+            }
+            else
+            {
+                Dictionary<string, string> vals = new Dictionary<string, string>()
+                {
+                    ["\"Surname\""] = textBoxSurnameInput.Text,
+                    ["\"Name\""] = textBoxNameInput.Text,
+                    ["\"Middle_Name\""] = textBoxMiddleNameInput.Text,
+                    ["\"Passport_ID\""] = maskedtextBoxInputPassID.Text,
+                    ["\"INN\""] = maskedTextBoxInputINN.Text
+                };
+                vals = PrepareData(vals);
+                try
+                {
+                    if (Text == "Редактирование")
+                    {
+                        Authorization.DBC.Update("\"Employee\"", Config.valueFromTableForEdit["ID"], vals);
+                        MessageBox.Show("Запись успешно обновлена.");
+                        ClearForm();
+                    }
+                    else
+                    {
+                        Authorization.DBC.Insert("\"Employee\"", vals);
+                        MessageBox.Show("Сотрудник добавлен.");
+                        ClearForm();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+                this.Close();
+            }
+        }
+
+        private void ButtonAddPosition_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
---
+++ Security_Agency/AddPosition.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Security_Agency
+{
+    partial class AddPosition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCancelAddClient = new System.Windows.Forms.Button();
+            this.buttonAddPosition = new System.Windows.Forms.Button();
+            this.labelSurnameInput = new System.Windows.Forms.Label();
+            this.textBoxPositionTitleInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCancelAddClient
+            // 
+            this.buttonCancelAddClient.Location = new System.Drawing.Point(135, 90);
+            this.buttonCancelAddClient.Name = "buttonCancelAddClient";
+            this.buttonCancelAddClient.Size = new System.Drawing.Size(117, 38);
+            this.buttonCancelAddClient.TabIndex = 29;
+            this.buttonCancelAddClient.Text = "Отмена";
+            this.buttonCancelAddClient.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddPosition
+            // 
+            this.buttonAddPosition.Location = new System.Drawing.Point(12, 90);
+            this.buttonAddPosition.Name = "buttonAddPosition";
+            this.buttonAddPosition.Size = new System.Drawing.Size(117, 38);
+            this.buttonAddPosition.TabIndex = 28;
+            this.buttonAddPosition.Text = "Добавить должность";
+            this.buttonAddPosition.UseVisualStyleBackColor = true;
+            // 
+            // labelSurnameInput
+            // 
+            this.labelSurnameInput.AutoSize = true;
+            this.labelSurnameInput.Location = new System.Drawing.Point(50, 31);
+            this.labelSurnameInput.Name = "labelSurnameInput";
+            this.labelSurnameInput.Size = new System.Drawing.Size(141, 13);
+            this.labelSurnameInput.TabIndex = 31;
+            this.labelSurnameInput.Text = "Наименование должности";
+            // 
+            // textBoxPositionTitleInput
+            // 
+            this.textBoxPositionTitleInput.Location = new System.Drawing.Point(12, 47);
+            this.textBoxPositionTitleInput.Name = "textBoxPositionTitleInput";
+            this.textBoxPositionTitleInput.Size = new System.Drawing.Size(227, 20);
+            this.textBoxPositionTitleInput.TabIndex = 30;
+            // 
+            // AddPosition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 140);
+            this.Controls.Add(this.labelSurnameInput);
+            this.Controls.Add(this.textBoxPositionTitleInput);
+            this.Controls.Add(this.buttonCancelAddClient);
+            this.Controls.Add(this.buttonAddPosition);
+            this.Name = "AddPosition";
+            this.Text = "AddPosition";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancelAddClient;
+        private System.Windows.Forms.Button buttonAddPosition;
+        private System.Windows.Forms.Label labelSurnameInput;
+        private System.Windows.Forms.TextBox textBoxPositionTitleInput;
+    }
+}
