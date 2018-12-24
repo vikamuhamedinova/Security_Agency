4102c57bcfbe7708ec2ab53f3c97c7a894346434
Dartkyn <alexeypantera96@gmail.com>
2018-12-25 00:29:41 +0700

    Формы добавления платежей и штрафов

---
+++ Security_Agency/AddPaymentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Security_Agency
+{
+    public partial class AddPaymentType : Form
+    {
+        public AddPaymentType()
+        {
+            InitializeComponent();
+        }
+    }
+}
