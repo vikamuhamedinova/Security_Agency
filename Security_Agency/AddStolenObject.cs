c396f13f0c88aa7dc53e155d38fcc50019023535
Dartkyn <alexeypantera96@gmail.com>
2018-12-27 19:26:38 +0700

    Отредактированы ураденное имущество и задержанные лица

--- Security_Agency/AddStolenObject.cs
+++ Security_Agency/AddStolenObject.cs
@@ -12,9 +12,106 @@ namespace Security_Agency
 {
     public partial class AddStolenObject : Form
     {
+        private AddCall mainForm;
         public AddStolenObject()
         {
             InitializeComponent();
+        }
+        //
+        public AddStolenObject(AddCall form)
+        {
+            InitializeComponent();
+            ClearForm();
+            mainForm = form;
+        }
+        //
+        private void ClearForm()
+        {
+            textBoxPropertyNameInput.Text = "";
+            textBoxPropertyAmmountInput.Text = "";
+        }
+        // преобразование к строке
+        private string ConvertToStringDB(string text)
+        {
+            return "'" + text + "'";
+        }
+        // убирает все пустые значения, выполняет преобразования к строке или к дате
+        private Dictionary<string, string> PrepareData(Dictionary<string, string> vals)
+        {
+            IList<string> keysToRemove = new List<string>();
+            foreach (var pair in vals)
+            {
+                if (vals[pair.Key] == "")
+                    keysToRemove.Add(pair.Key);
+            }
+            foreach (var key in keysToRemove)
+            {
+                vals.Remove(key);
+            }
+            var newDict = new Dictionary<string, string>();
+            foreach (var key in vals.Keys)
+            {
+                newDict.Add(key, ConvertToStringDB(vals[key]));
+            }
+            return newDict;
+        }
+        //
+        private void AddApartment_Load(object sender, EventArgs e)
+        {
+            if (Text == "Редактирование")
+            {
+                this.buttonAddStolenProperty.Text = "Сохранить";
+                textBoxPropertyNameInput.Text = Config.valueFromTableForEdit["Наименование имущества"];
+                textBoxPropertyNameInput.Text = Config.valueFromTableForEdit["Количество"];
+            }
+            else
+                ClearForm();
+        }
+        // 
+        private void ButtonAddApartment_Click(object sender, EventArgs e)
+        {
+            if (textBoxPropertyNameInput.Text == ""|| textBoxPropertyAmmountInput.Text == "")
+            {
+                MessageBox.Show("Не заполнено одно из обязательных полей");
+            }
+            else
+            {
+                Dictionary<string, string> vals = new Dictionary<string, string>()
+                {
+                    ["\"Name\""] = textBoxPropertyNameInput.Text,
+                    ["\"Amount\""] = textBoxPropertyAmmountInput.Text 
+                };
+                vals = PrepareData(vals);
+                try
+                {
+                    if (Text == "Редактирование")
+                    {
+                        Authorization.DBC.Update("\"Stolen_Object\"", Config.valueFromTableForEdit["ID"], vals);
+                        MessageBox.Show("Запись успешно обновлена.");
+                        ClearForm();
+                    }
+                    else
+                    {
+                        Authorization.DBC.Insert("\"Stolen_Object\"", vals);
+                        MessageBox.Show("Имущество добавлено.");
+                        ClearForm();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+                this.Close();
+            }
+        }
+        // 
+        private void ButtonCancelAddApartmentClick(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите отменить добавление?", "Отмена добавления",
+                            MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
         }
     }
 }
